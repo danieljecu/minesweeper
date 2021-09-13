8ae33d58731a88f6d8fe5547ada931ba5493f465
danieljecu <>
2021-09-13 18:10:15 +0100

    hiddenBoard

--- src/Minesweeper.ts
+++ src/Minesweeper.ts
@@ -2,25 +2,32 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max);
   }
   
-  // enum TileStatus {
-  //   HIDDEN = "hidden",
-  //   MINE = "mine",
-  // };
+  export enum TileStatus {
+    EMPTY = 0,
+    HIDDEN = "hidden",
+    NUMBER = "number",
+    MINE = 9,
+    REVEAL = "reveal"
+  } 
+  
+  function isBomb(cell) {
+    return cell === TileStatus.MINE;
+  }
   
   /*
-  FIRST PART -- LOGIC
-  1.Populate board -- bombs, 
-  2.calculate adiacent box, precalculate numbers
-  
-  
-  SECOND PART -- UI
-  1.UI grid
-  2.Left click on tiles -- reveal tiles
-  
-  3.Right click -- mark tiles
-  4.Check for win/lose
-  
-  */
+    FIRST PART -- LOGIC
+    1.Populate board -- bombs
+    2.calculate adiacent box, precalculate numbers
+    
+    
+    SECOND PART -- UI
+    1.UI grid
+    2.Left click on tiles -- reveal tiles
+    
+    3.Right click -- mark tiles
+    4.Check for win/lose
+    
+    */
   
   const adiacentBoxIndexesInsideGrid = (i, j, rows, columns) => {
     let arrOfIndexesInsideGrid = [
@@ -43,39 +50,39 @@ function getRandomInt(max) {
     );
   };
   
-  export function assignMines(minesCount = 3, rows = 5, colums = 5) {
+  export function generateGrid(minesCount = 3, rows = 5, colums = 5) {
     //generate grid
     //calc adiacentBox // border or not border
-    let g = [];
+    let g = [],
+      hidden = [];
+  
     for (let i = 0; i < rows; i++) {
       g[i] = new Array(colums).fill(0);
+      // hidden[i] = new Array(colums).fill(TileStatus.HIDDEN);
     }
-    // console.table(g);
+  
     let minesAssigned = 0;
     while (minesAssigned < minesCount) {
       //mines 3
       // console.table(g, "mines", minesAssigned);
       const x = getRandomInt(rows);
       const y = getRandomInt(colums);
-      if (g[x][y] !== 9) {
-        g[x][y] = 9;
+      if (g[x][y] !== TileStatus.MINE) {
+        g[x][y] = TileStatus.MINE;
         minesAssigned += 1;
       }
     }
-  
-    for (let i = 0; i < rows ; i++) {
-      for (let j = 0; j < colums ; j++) {
-      
-        if (g[i][j] === 9) {
+    // precalculate nhood for non bombs CELLs
+    for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < colums; j++) {
+        if (g[i][j] === TileStatus.MINE) {
           break;
         }
-  
-          //if i=0 or i=rown //missing  top or button
+        //if i=0 or i=rown //missing  top or button
         //if j=0 or j=coln //missing left or rignt
         // (i,j, rows, columns) => [valid box elements] => filter (i,j => g[i][j] ===)
-  
-        g[i][j] = adiacentBoxIndexesInsideGrid(i, j, rows, colums).filter(
-          (e) => g[e[0]][e[1]] === 9
+        g[i][j] = adiacentBoxIndexesInsideGrid(i, j, rows, colums).filter((e) =>
+          isBomb(g[e[0]][e[1]])
         ).length;
       }
     }
--- src/index.js
+++ src/index.js
@@ -1,20 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
-import { assignMines } from "./Minesweeper";
+import { generateGrid, TileStatus } from "./Minesweeper";
 
 import "./styles.css";
 
-function isBomb(cellNum) {
-  return cellNum === 9;
-}
-function MinesweeperUI() {
-  // const [grid, setGrid]= useState([[]])
+const ROWS = 5;
+const COLUMS = 5;
+const MINES = 4;
 
-  const board = assignMines(4, 5, 5);
-  const gameBoard = board.map((row) => (
-    <div className="row">
-      {row.map((box) => (
-        <div className={`box`}>{box}</div>
+const board = generateGrid(MINES, ROWS, COLUMS);
+
+function MinesweeperUI() {
+  const [grid, setGrid] = useState(
+    Array.from({ length: ROWS }, () =>
+      Array.from({ length: COLUMS }, () => TileStatus.HIDDEN)
+    )
+  );
+
+  const handleChange = (row, column, event) => {
+    let copy = [...grid];
+    copy[row][column] = board[row][column];
+    console.log(copy[row][column]);
+
+    setGrid(copy);
+  };
+
+  // useEffect(() => {
+  //   return;
+  // }, [grid]);
+
+  // const gameBoard = board.map((row) => (
+  //   <div className="row">
+  //     {row.map((box) => (
+  //       <div className={`box`}>{box}</div>
+  //     ))}
+  //   </div>
+  // ));
+
+  const hiddenBoard = grid.map((row, rowIndex) => (
+    <div key={rowIndex} className="row">
+      {row.map((box, columnIndex) => (
+        <div
+          className="box"
+          key={columnIndex}
+          onClick={(e) => handleChange(rowIndex, columnIndex, e)}
+        >
+          {box === TileStatus.HIDDEN
+            ? TileStatus.HIDDEN
+            : board[rowIndex][columnIndex]}
+        </div>
       ))}
     </div>
   ));
@@ -22,7 +56,8 @@ function MinesweeperUI() {
   return (
     <div className="App">
       <h1>Minesweeper</h1>
-      <div className="gameBoard">{gameBoard}</div>
+      {/* <div className="gameBoard">{gameBoard}</div> */}
+      {hiddenBoard}
     </div>
   );
 }
